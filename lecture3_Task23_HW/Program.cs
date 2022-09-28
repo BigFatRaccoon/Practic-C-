384f34d19fb203c7b2499bbf4bdaaad91050e728
Iliya Palych <iliya-paly4@yandex.ru>
2022-09-28 22:24:56 +0400

    add lecture3_Task23_HW

---
+++ lecture3_Task23_HW/Program.cs
@@ -0,0 +1,17 @@
+﻿// Напишите программу, которая принимает на вход число N и выдает таблицу кубов чисел от 1 до N
+
+Console.WriteLine("Введите число N: ");
+int num = Convert.ToInt32(Console.ReadLine());
+
+void Cube(int number)
+{
+    int count = 1;
+    while (count <= num)
+    {
+        Console.WriteLine($"|{count, 3}|  {count * count * count, 5} |");
+        count++;
+    }
+}
+
+if (num > 0) Cube(num);
+else Console.WriteLine("Введено верное значение! ");
